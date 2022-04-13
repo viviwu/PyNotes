6845209484f2ac48c949b1ff6676a85af74eb8e2
vivi wu <vivi705@qq.com>
2022-04-13 22:25:30 +0800

    Create playground.py
    1

---
+++ py3samples/playground.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: viviwu
+@email: vivi705@icloud.com
+@file: playground.py
+@time: 2021/12/13 7:34 下午
+@desc: Copyright © viviwu All rights reserved.
+
+"""
+import sys
+
+print(sys.platform)
+# print(sys.)
